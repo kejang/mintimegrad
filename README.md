feae7ec119db97de733cf131293f1a72e24bb950
Kwang Eun Jang <kejang@stanford.edu>
2022-06-26 18:59:08 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python Wrapper of Miki Lustig's tOptGrad
+
+The original source is available at [Miki Lustig's home page](https://people.eecs.berkeley.edu/~mlustig/Software.html).
+
+## License
+
+This is just a simple Python wrapper of the [original software](https://people.eecs.berkeley.edu/~mlustig/Software.html). All credits and the copyright should go to original authors.
---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+import os
+base_path = os.path.dirname(__file__)
+
+ext_modules = [
+    Pybind11Extension(
+        "mintimegrad",
+        ["mintimegrad/mtg.cpp"],
+    ),
+]
+
+setuptools.setup(
+    name="mintimegrad",
+    version="0.0.1",
+    author="Kwang Eun Jang",
+    author_email="kejang@stanford.edu",
+    description="Python Wrapper of Miki Lustig's tOptGrad",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kejang/mintimegrad",
+    project_urls={
+        "Bug Tracker": "https://github.com/kejang/mintimegrad/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "pybind11 >=2.6.0",
+    ],
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+)
