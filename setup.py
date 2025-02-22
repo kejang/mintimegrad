14dac303618e095ac72f16f717a98aa0e86c33a7
Kwang Eun Jang <kejang@stanford.edu>
2025-02-21 18:06:21 -0800

    remove version.py

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ import os
 from pathlib import Path
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-module_path = Path(os.path.abspath(__file__)).parent.absolute()
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
 
-ver = {}
-with open(module_path.joinpath('version.py')) as ver_file:
-    exec(ver_file.read(), ver)
+module_path = Path(os.path.abspath(__file__)).parent.absolute()
+package_name = "mintimegrad"
+
+try:
+    pkg_version = version(package_name)
+except Exception:
+    pkg_version = "0.0.6"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -22,8 +29,8 @@ ext_modules = [
 ]
 
 setuptools.setup(
-    name="mintimegrad",
-    version=ver['__version__'],
+    name=package_name,
+    version=pkg_version,
     author="Kwang Eun Jang",
     author_email="kejang@stanford.edu",
     description="Python Wrapper of Miki Lustig's tOptGrad",
@@ -33,17 +40,9 @@ setuptools.setup(
     project_urls={
         "Bug Tracker": "https://github.com/kejang/mintimegrad/issues",
     },
-    classifiers=[
-        "Programming Language :: Python :: 3.10",
-        "Operating System :: POSIX :: Linux",
-    ],
     package_dir={"": "."},
     packages=setuptools.find_packages(where="."),
     python_requires=">=3.10",
-    install_requires=[
-        "numpy >= 1.17.3",
-        "pybind11 >=2.6.0",
-    ],
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
