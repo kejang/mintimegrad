03497bb0aaefce799f72b8c4e00a1ed312bba16a
Kwang Eun Jang <kejang@stanford.edu>
2025-02-21 18:06:23 -0800

    Bump version: 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ package_name = "mintimegrad"
 try:
     pkg_version = version(package_name)
 except Exception:
-    pkg_version = "0.0.6"
+    pkg_version = "0.0.7"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
